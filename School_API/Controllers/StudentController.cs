c4bede595713e0ecc9f287f95be58fc7ec38ede4
BetMarmen <betelmarquez137@gmail.com>
2024-06-04 08:28:28 -0600

    Agregar archivos de proyecto.

---
+++ School_API/Controllers/StudentController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using School_API.Data;
+using School_API.Dto;
+
+
+namespace School_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly SchoolContext _context;
+        private readonly ILogger<StudentController> _logger;
+        private readonly IMapper _mapper;
+
+        public StudentController(SchoolContext context, ILogger<StudentController> logger, IMapper mapper)
+        {
+            _context = context;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<StudentDto>> GetStudent()
+        {
+            try
+            {
+                _logger.LogInformation("Obteniendo los estudiantes");
+
+                var students = await _context.Students.ToListAsync();
+
+                return Ok(_mapper.Map<IEnumerable<StudentDto>>(students));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error al obtener los estudiantes: {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error interno del servidor al obtener los estudiantes.");
+            }
+        }
+    }
+}
---
+++ School_API/Dto/StudentUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace School_API.Dto
+{
+    public class StudentUpdateDto
+    {
+        [Required]
+        public int StudentId { get; set; }
+        [StringLength(50)]
+        public string? Name { get; set; }
+        public bool Registered { get; set; }
+    }
+}
